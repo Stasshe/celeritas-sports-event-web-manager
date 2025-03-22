57036a639d34ffb5d4611dcb1df20befd6d62e36
Stasshe <egnm9stasshe@gmail.com>
2025-03-22 15:35:55 +0000

    一時ファイルを使用して重複を削除し、翻訳結果を保存するようにスクリプトを更新

--- extract_i18n.sh
+++ extract_i18n.sh
@@ -9,8 +9,14 @@ output_file="translation.txt"
 # src/ディレクトリ内のすべてのファイルをループ
 find src/ -type f | while read file; do
   # 各ファイルの中身からt()の中身を抽出
-  # 抽出した文字列が'を含み、かつ4文字以上の場合のみ、translation.txtに追加
-  grep -oP 't\(\K[^\)]+' "$file" | grep "'" | awk 'length($0) >= 4' >> "$output_file"
+  # 抽出した文字列が'を含み、かつ4文字以上の場合のみ、一時ファイルに追加
+  grep -oP 't\(\K[^\)]+' "$file" | grep "'" | awk 'length($0) >= 4' >> temp.txt
 done
+
+# 重複を削除して、結果をtranslation.txtに保存
+sort temp.txt | uniq > "$output_file"
+
+# 一時ファイルを削除
+rm temp.txt
 
 echo "抽出が完了しました。結果は $output_file に保存されました。"
